4406e91dc1c7732e606fb24bd9b220d32a889a48
arun <arun.t@bluecloudsoftech.com>
2025-10-24 21:45:11 +0000

    min

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,9 @@
 mod db;
 mod commands;
 mod server;
+
 use crate::db::Database;
 use std::sync::Arc;
-
-
-
-// #[global_allocator]
-// static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
-// #[tokio::main]
-// async fn main() -> std::io::Result<()> {
-
-//     let addr = "127.0.0.1:2002";
-//     let database = Database::new();
-    
-//     println!("Starting FlashTree server...");
-//     server::start(addr, database).await
-// }
-
-
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
